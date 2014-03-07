c1c8f9ec45d724041e8e3852b7beb8a9ab357dbb
John Sextro <John.Sextro@gmail.com>
2014-03-07 11:10:56 -0600

    Refactored all the tests to be faster by reducing the wait times used to test the clock fuction.

--- spec/clock-spec.js
+++ spec/clock-spec.js
@@ -5,32 +5,32 @@ describe("runTheClock", function () {
   it("time should be greater than 0", function () {
 	clock.reset();
 	clock.start();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	var elapsedTime = clock.getTime();
 	expect(elapsedTime).toBeGreaterThan(-1);
   });
 
   it("if the clock hasn't stated the time returned should be -1", function () {
 	clock.reset();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	expect(clock.getTime()).toEqual(-1);
   });
 
   it("resetting the clock should make time -1", function () {
 	clock.reset();
 	clock.start();
-	waiter.sleep(30);
+	waiter.sleep(5);
 	clock.reset();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	expect(clock.getTime()).toEqual(-1);
   });
 
   it("time should always be increasing", function () {
     clock.start();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	var elapsedTime = clock.getTime();
 	expect(elapsedTime).toBeGreaterThan(0);
-	waiter.sleep(10);
+	waiter.sleep(5);
 	nextElapsedTime = clock.getTime();
 	expect(nextElapsedTime).toBeGreaterThan(elapsedTime);
   });
@@ -38,13 +38,13 @@ describe("runTheClock", function () {
   it("can not start the clock once started", function () {
 	clock.reset();
     clock.start();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	var elapsedTime = clock.getTime();
 	expect(elapsedTime).toBeGreaterThan(0);
 	clock.start();
-	waiter.sleep(10);
+	waiter.sleep(5);
 	nextElapsedTime = clock.getTime();
-	expect(nextElapsedTime).toBeGreaterThan(10);
+	expect(nextElapsedTime).toBeGreaterThan(9);
 	expect(nextElapsedTime).toBeGreaterThan(elapsedTime);
   });
 
@@ -52,20 +52,20 @@ describe("runTheClock", function () {
    	clock.reset();
 	
 	clock.start();
-	waiter.sleep(20)
+	waiter.sleep(5)
 	clock.pause();
-	waiter.sleep(20);
-	expect(clock.getTime()).toBeLessThan(25);
+	waiter.sleep(5);
+	expect(clock.getTime()).toBeLessThan(10);
   });
 
   it("able to pause and resume the clock", function () {
    	clock.reset();
 	
 	clock.start();
-	waiter.sleep(20)
+	waiter.sleep(5)
 	clock.pause();
-	waiter.sleep(20);
+	waiter.sleep(5);
 	clock.resume();
-	expect(clock.getTime()).toBeLessThan(25);
+	expect(clock.getTime()).toBeLessThan(10);
   });
 });    
