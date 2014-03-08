3ffa5a4c79614563cd4928d583d6e741f4ceb164
johnsextro <john.sextro@gmail.com>
2014-03-08 11:00:39 -0600

    Transformed the clock into a prototype object that can be constructed and used multiple times. This will be important because multiple podclock users could have different clocks running simultaneously.

--- controller/clock.js
+++ controller/clock.js
@@ -1,42 +1,48 @@
-var startTime = -1;
-var pausedTime = 0;
-var currentlyPaused = false;
-var startPauseTime = -1;
+function Podclock () {
 
-exports.start = function(startTime) {
-	if (this.startTime == -1) {
-		this.startTime = new Date().getTime();
-	}
-}
-
-exports.getTime = function() {
-	if(this.startTime == -1) {
-		return this.startTime;
-	} else if (this.currentlyPaused) {
-		return this.startPauseTime - this.startTime;
-	} else {
-		return (new Date().getTime() - this.pausedTime) - this.startTime;
-	}
-}
-
-exports.pause = function() {
-	if (!this.currentlyPaused) {
-		this.currentlyPaused = true;
-		this.startPauseTime = new Date().getTime();
-	}
-}
-
-exports.resume = function() {
-	if (this.currentlyPaused) {
-		this.currentlyPaused = false;
-		this.pausedTime += new Date().getTime() - this.startPauseTime;
-		this.startPauseTime = -1;
-	}
-}
-
-exports.reset = function() {
 	this.startTime = -1;
 	this.pausedTime = 0;
 	this.currentlyPaused = false;
 	this.startPauseTime = -1;
+
+	this.start = function(startTime) {
+		if (this.startTime == -1) {
+			this.startTime = new Date().getTime();
+		}
+	};
+
+	this.getTime = function() {
+		if(this.startTime == -1) {
+			return this.startTime;
+		} else if (this.currentlyPaused) {
+			return this.startPauseTime - this.startTime;
+		} else {
+			return (new Date().getTime() - this.pausedTime) - this.startTime;
+		}
+	};
+
+	this.pause = function() {
+		if ((this.startTime != -1) && (!this.currentlyPaused)) {
+			this.currentlyPaused = true;
+			this.startPauseTime = new Date().getTime();
+		}
+	};
+
+	this.resume = function() {
+		if (this.currentlyPaused) {
+			this.currentlyPaused = false;
+			this.pausedTime += new Date().getTime() - this.startPauseTime;
+			this.startPauseTime = -1;
+		}
+	};
+
+	this.reset = function() {
+		this.startTime = -1;
+		this.pausedTime = 0;
+		this.currentlyPaused = false;
+		this.startPauseTime = -1;
+	};
+
 }
+
+module.exports = Podclock;
