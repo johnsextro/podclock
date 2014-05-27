3539e46201177fbe9fe339f345e4bb446bee98d8
John Sextro <John.Sextro@gmail.com>
2014-05-27 16:56:53 -0500

    Refactored the test so that it created its own server before running and shuts down the server after running. This makes it easier to run the tests because you don't have to have the server running somewhere else.  Also did some refactoring in order to make it easier to start the server via the tests.

--- spec/clock-socket-spec.js
+++ spec/clock-socket-spec.js
@@ -1,15 +1,24 @@
 var io = require('socket.io-client');
 var clockSocket = require('../controller/clock-socket.js');
 var waiter = require("./waiter.js");
-
+var appBoot = require('../appBoot.js');
+var clockSocket = require('../controller/clock-socket.js');
 
 describe("test the clock socket events", function () {
 
 	var socket;
 	var cohost;
+	var server;
 
 	beforeEach(function(done) {
 	    // Setup
+	    var app = appBoot.init();
+	    server = app.listen(3000, function(){
+		  console.log("Express server listening on port " + app.address().port, app.settings.env);
+		  done();
+		});
+		clockSocket.registerSocketEvents(server);
+
 	    socket = io.connect('http://localhost:3000', {
 	        'reconnection delay' : 0
 	        , 'reopen delay' : 0
@@ -39,14 +48,32 @@ describe("test the clock socket events", function () {
 	afterEach(function(done) {
 	    // Cleanup
 	    if(socket.socket.connected) {
-	        console.log('disconnecting...');
+	        console.log('disconnecting host...');
+	        socket.emit('resetClock');
 	        socket.disconnect();
-	        cohost.disconnect();
+	        done();
 	    } else {
 	        // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
 	        console.log('no connection to break...');
 	    }
+	    if(cohost.socket.connected) {
+	        console.log('disconnecting cohost...');
+	        cohost.disconnect();
+	        done();
+	    } else {
+	        // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
+	        console.log('no connection to break...');
+	    }
+	    server.close();
 	    done();
+	});
+
+	it("ensure client is getting time updates", function(done) {
+		socket.on('timeUpdate', function(runningTime) {
+			expect(runningTime).toMatch("00:00:01");
+			done();
+		});
+		socket.emit('clockClick');
 	});
 
 	it("testing communication of title suggestions", function (done) {
