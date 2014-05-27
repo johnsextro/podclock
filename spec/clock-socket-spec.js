4e48dea7fbbc6dbc38ed2bd87b7bd036d49fc5b7
John Sextro <John.Sextro@gmail.com>
2014-05-27 13:53:14 -0500

    Have test that are running async to await the appropriate reponse.  This is testing communication between the host and a co-host.

--- spec/clock-socket-spec.js
+++ spec/clock-socket-spec.js
@@ -2,6 +2,7 @@ var io = require('socket.io-client');
 var clockSocket = require('../controller/clock-socket.js');
 var waiter = require("./waiter.js");
 
+
 describe("test the clock socket events", function () {
 
 	var socket;
@@ -27,11 +28,6 @@ describe("test the clock socket events", function () {
 	        console.log('cohost connected...');
 	        done();
 	    });
-	    cohost.on('addTitleSuggestion', function(data) {
-			console.log("Title is " + data);
-			expect(data).toEqual('wrongtest title suggestion');
-			done();	
-		});
 	    socket.on('disconnect', function() {
 	        console.log('host disconnected...');
 	    })
@@ -40,21 +36,33 @@ describe("test the clock socket events", function () {
 	    })
 	});
 
-	// afterEach(function(done) {
- //        // Cleanup
- //        if(socket.socket.connected) {
- //            console.log('disconnecting...');
- //            socket.disconnect();
- //            cohost.disconnect();
- //        } else {
- //            // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
- //            console.log('no connection to break...');
- //        }
- //        done();
- //    });
+	afterEach(function(done) {
+	    // Cleanup
+	    if(socket.socket.connected) {
+	        console.log('disconnecting...');
+	        socket.disconnect();
+	        cohost.disconnect();
+	    } else {
+	        // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
+	        console.log('no connection to break...');
+	    }
+	    done();
+	});
 
-	it("connect to this socket event handler", function () {
-		socket.emit('titleSuggested', 'test title suggestions');
+	it("testing communication of title suggestions", function (done) {
+		cohost.on('addTitleSuggestion', function(data) {
+			expect(data).toMatch('test title suggestion');
+			done();	
+		});
+		socket.emit('titleSuggested', 'test title suggestion');
+	});
+
+	it("testing notification to add an event button", function (done) {
+		cohost.on('addShowEventButton', function(data) {
+			expect(data).toMatch('test button name');
+			done();	
+		});
+		socket.emit('addShowEventButton', 'test button name');
 	});
 
 });    
