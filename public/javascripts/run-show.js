79040cad7f0f34ec2cc11cd87fc63165f3ec07c5
John Sextro <John.Sextro@gmail.com>
2014-05-31 15:55:40 -0500

    Wired authorization to update the username on the navbar.

--- public/javascripts/run-show.js
+++ public/javascripts/run-show.js
@@ -79,6 +79,30 @@ function createHandlersForSocketMessages() {
     createEventButton(buttonText);
   });
 }
+
+function signinCallback(authResult) {
+  if (authResult['status']['signed_in']) {
+    // Update the app to reflect a signed in user
+    // Hide the sign-in button now that the user is authorized, for example:
+    document.getElementById('signinButton').setAttribute('style', 'display: none');
+    gapi.client.load('plus','v1', function(){
+     var request = gapi.client.plus.people.get({
+       'userId': 'me'
+     });
+     request.execute(function(resp) {
+       $('#displayName').text(resp.displayName);
+     });
+    });
+  } else {
+    // Update the app to reflect a signed out user
+    // Possible error values:
+    //   "user_signed_out" - User is signed-out
+    //   "access_denied" - User denied access to your app
+    //   "immediate_failed" - Could not automatically log in the user
+    console.log('Sign-in state: ' + authResult['error']);
+  }
+
+}
  
 $(function() {
   createHandlersForSocketMessages();
