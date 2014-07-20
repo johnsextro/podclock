50c8bf3b6a0ae601f3371602b9f901ef0cc36e99
Family <Family@Familys-iMac.local>
2014-07-20 11:22:23 -0500

    The tests are passing on the iMac.  I am going to check them in an see if they run successfully on Codeship.

--- spec/clock-socket-spec.js
+++ spec/clock-socket-spec.js
@@ -23,25 +23,25 @@ describe("test the clock socket events", function () {
 	        , 'reopen delay' : 0
 	        , 'force new connection' : true
 	    });
-	    // cohost = io.connect('http://localhost:3000', {
-	    //     'reconnection delay' : 0
-	    //     , 'reopen delay' : 0
-	    //     , 'force new connection' : true
-	    // });
+	    cohost = io.connect('http://localhost:3000', {
+	        'reconnection delay' : 0
+	        , 'reopen delay' : 0
+	        , 'force new connection' : true
+	    });
 	    host.on('connect', function() {
 	    	console.log('connect');
 	        done();
 	    });
-	    // cohost.on('connect', function() {
-	    //     done();
-	    // });
+	    cohost.on('connect', function() {
+	        done();
+	    });
 	    host.on('disconnect', function() {
 	        done();
 	    });
-	    // cohost.on('disconnect', function() {
-	    //     done();
-	    // });
-	    // host.emit('claimHostRole');
+	    cohost.on('disconnect', function() {
+	        done();
+	    });
+	    host.emit('claimHostRole');
 	});
 
 	afterEach(function(done) {
@@ -54,10 +54,10 @@ describe("test the clock socket events", function () {
 	        // There will not be a connection unless you have done() in beforeEach, socket.on('connect'...)
 	        console.log('no connection to break...');
 	    }
-	    // if(cohost.socket.connected) {
-	    //     cohost.disconnect();
-	    //     done();
-	    // }
+	    if(cohost.socket.connected) {
+	        cohost.disconnect();
+	        done();
+	    }
 	    server.close();
 	    done();
 	});
@@ -70,59 +70,59 @@ describe("test the clock socket events", function () {
 		host.emit('clockClick');
 	});
 
-	// it("When the host starts the clock and cohosts begin getting clock updates", function(done) {
-	// 	cohost.on('timeUpdate', function(runningTime) {
-	// 		expect(runningTime).toMatch("00:00:01");
-	// 		done();
-	// 	});
-	// 	host.emit('clockClick');
-	// });
+	it("When the host starts the clock and cohosts begin getting clock updates", function(done) {
+		cohost.on('timeUpdate', function(runningTime) {
+			expect(runningTime).toMatch("00:00:01");
+			done();
+		});
+		host.emit('clockClick');
+	});
 
-	// it("host resets the clock and cohost receives reset event", function(done) {
-	// 	cohost.on('resetAllShowData', function() {
-	// 		expect(true).toBeTruthy();
-	// 		done();
-	// 	});
-	// 	host.emit('resetClock');
-	// });
+	it("host resets the clock and cohost receives reset event", function(done) {
+		cohost.on('resetAllShowData', function() {
+			expect(true).toBeTruthy();
+			done();
+		});
+		host.emit('resetClock');
+	});
 
-	// it("host suggests a title and cohost receives title suggestion", function (done) {
-	// 	cohost.on('addTitleSuggestion', function(data) {
-	// 		expect(data).toMatch('test title suggestion');
-	// 		done();	
-	// 	});
-	// 	host.emit('titleSuggested', 'test title suggestion');
-	// });
+	it("host suggests a title and cohost receives title suggestion", function (done) {
+		cohost.on('addTitleSuggestion', function(data) {
+			expect(data).toMatch('test title suggestion');
+			done();	
+		});
+		host.emit('titleSuggested', 'test title suggestion');
+	});
 
-	// it("cohost suggests a title and host receives title suggestion", function (done) {
-	// 	host.on('addTitleSuggestion', function(data) {
-	// 		expect(data).toMatch('test title suggestion');
-	// 		done();	
-	// 	});
-	// 	cohost.emit('titleSuggested', 'test title suggestion');
-	// });
+	it("cohost suggests a title and host receives title suggestion", function (done) {
+		host.on('addTitleSuggestion', function(data) {
+			expect(data).toMatch('test title suggestion');
+			done();	
+		});
+		cohost.emit('titleSuggested', 'test title suggestion');
+	});
 
-	// it("host creates a new event button and cohost receives add button event", function (done) {
-	// 	cohost.on('addShowEventButton', function(data) {
-	// 		expect(data).toMatch('test button name');
-	// 		done();	
-	// 	});
-	// 	host.emit('addShowEventButton', 'test button name');
-	// });
+	it("host creates a new event button and cohost receives add button event", function (done) {
+		cohost.on('addShowEventButton', function(data) {
+			expect(data).toMatch('test button name');
+			done();	
+		});
+		host.emit('addShowEventButton', 'test button name');
+	});
 
-	// it("host logs event and event is sent to host and cohost", function (done) {
-	// 	cohost.on('addEventTimeCode', function(data) {
-	// 		expect(data).toContain('test time code @');
-	// 		done();	
-	// 	});
-	// 	host.on('addEventTimeCode', function(data) {
-	// 		expect(data).toContain('test time code @');
-	// 		done();	
-	// 	});
-	// 	host.emit('clockClick');
-	// 	waiter.sleep(1);
-	// 	host.emit('showEventTimeCode', 'test time code');
-	// });
+	it("host logs event and event is sent to host and cohost", function (done) {
+		cohost.on('addEventTimeCode', function(data) {
+			expect(data).toContain('test time code @');
+			done();	
+		});
+		host.on('addEventTimeCode', function(data) {
+			expect(data).toContain('test time code @');
+			done();	
+		});
+		host.emit('clockClick');
+		waiter.sleep(1);
+		host.emit('showEventTimeCode', 'test time code');
+	});
 
 	// it("only the host can start the clock", function (done) {
 	// 	cohost.on('notHost', function(message) {
