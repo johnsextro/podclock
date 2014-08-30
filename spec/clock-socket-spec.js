fda459517b99264970bf536067389cf917f8a067
John Sextro <john.sextro@gmail.com>
2014-08-30 10:09:12 -0500

    Ugh. tests always pass on codeship. not worrying about it anymore right now.

--- spec/clock-socket-spec.js
+++ spec/clock-socket-spec.js
@@ -78,7 +78,7 @@ describe("test the clock socket events", function () {
 
 	it("host resets the clock and cohost receives reset event", function(done) {
 		cohost.on('resetAllShowData', function() {
-			expect(false).toBeTruthy();
+			expect(true).toBeTruthy();
 			done();
 		});
 		host.emit('resetClock');
