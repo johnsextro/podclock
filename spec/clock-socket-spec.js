a1d69c3dbd339c8b332cf732905a9af9621ea7ed
John Sextro <john.sextro@gmail.com>
2014-08-31 07:55:40 -0500

    Reimplemented all tests

--- spec/clock-socket-spec.js
+++ spec/clock-socket-spec.js
@@ -68,71 +68,71 @@ describe("test the clock socket events", function () {
 		host.emit('clockClick');
 	});
 
-// 	it("host resets the clock and cohost receives reset event", function(done) {
-// 		cohost.on('resetAllShowData', function() {
-// 			expect(true).toBeTruthy();
-// 			done();
-// 		});
-// 		host.emit('resetClock');
-// 	});
+	it("host resets the clock and cohost receives reset event", function(done) {
+		cohost.on('resetAllShowData', function() {
+			expect(true).toBeTruthy();
+			done();
+		});
+		host.emit('resetClock');
+	});
 
-// 	it("host suggests a title and cohost receives title suggestion", function (done) {
-// 		cohost.on('addTitleSuggestion', function(data) {
-// 			expect(data).toMatch('test title suggestion');
-// 			done();	
-// 		});
-// 		host.emit('titleSuggested', 'test title suggestion');
-// 	});
+	it("host suggests a title and cohost receives title suggestion", function (done) {
+		cohost.on('addTitleSuggestion', function(data) {
+			expect(data).toMatch('test title suggestion');
+			done();	
+		});
+		host.emit('titleSuggested', 'test title suggestion');
+	});
 
-// 	it("cohost suggests a title and host receives title suggestion", function (done) {
-// 		host.on('addTitleSuggestion', function(data) {
-// 			expect(data).toMatch('test title suggestion');
-// 			done();	
-// 		});
-// 		cohost.emit('titleSuggested', 'test title suggestion');
-// 	});
+	it("cohost suggests a title and host receives title suggestion", function (done) {
+		host.on('addTitleSuggestion', function(data) {
+			expect(data).toMatch('test title suggestion');
+			done();	
+		});
+		cohost.emit('titleSuggested', 'test title suggestion');
+	});
 
-// 	it("host creates a new event button and cohost receives add button event", function (done) {
-// 		cohost.on('addShowEventButton', function(data) {
-// 			expect(data).toMatch('test button name');
-// 			done();	
-// 		});
-// 		host.emit('addShowEventButton', 'test button name');
-// 	});
+	it("host creates a new event button and cohost receives add button event", function (done) {
+		cohost.on('addShowEventButton', function(data) {
+			expect(data).toMatch('test button name');
+			done();	
+		});
+		host.emit('addShowEventButton', 'test button name');
+	});
 
-// 	it("host logs event and event is sent to host and cohost", function (done) {
-// 		cohost.on('addEventTimeCode', function(data) {
-// 			expect(data).toContain('test time code @');
-// 			done();	
-// 		});
-// 		host.on('addEventTimeCode', function(data) {
-// 			expect(data).toContain('test time code @');
-// 			done();	
-// 		});
-// 		host.emit('clockClick');
-// 		waiter.sleep(1);
-// 		host.emit('showEventTimeCode', 'test time code');
-// 	});
+	it("host logs event and event is sent to host and cohost", function (done) {
+		cohost.on('addEventTimeCode', function(data) {
+			expect(data).toContain('test time code @');
+			done();	
+		});
+		host.on('addEventTimeCode', function(data) {
+			expect(data).toContain('test time code @');
+			done();	
+		});
+		host.emit('clockClick');
+		waiter.sleep(1);
+		host.emit('showEventTimeCode', 'test time code');
+	});
 
-// 	// it("only the host can start the clock", function (done) {
-// 	// 	cohost.on('notHost', function(message) {
-// 	// 		expect(true).toBeTruthy();
-// 	// 		done();
-// 	// 	});
-// 	// 	cohost.emit('clockClick');
-// 	// });
+	// it("only the host can start the clock", function (done) {
+	// 	cohost.on('notHost', function(message) {
+	// 		expect(true).toBeTruthy();
+	// 		done();
+	// 	});
+	// 	cohost.emit('clockClick');
+	// });
 
-// 	// it("only the host can reset the clock", function (done) {
-// 	// 	cohost.on('notHost', function(message) {
-// 	// 		expect(true).toBeTruthy();
-// 	// 		done();
-// 	// 	});
-// 	// 	cohost.on('timeUpdate', function(runningTime) {
-// 	// 		expect(runningTime).toNotMatch("00:00:00");
-// 	// 		done();
-// 	// 	});
-// 	// 	host.emit('clockClick');
-// 	// 	waiter.sleep(1);
-// 	// 	cohost.emit('resetClock');
-// 	// });
+	// it("only the host can reset the clock", function (done) {
+	// 	cohost.on('notHost', function(message) {
+	// 		expect(true).toBeTruthy();
+	// 		done();
+	// 	});
+	// 	cohost.on('timeUpdate', function(runningTime) {
+	// 		expect(runningTime).toNotMatch("00:00:00");
+	// 		done();
+	// 	});
+	// 	host.emit('clockClick');
+	// 	waiter.sleep(1);
+	// 	cohost.emit('resetClock');
+	// });
 });
