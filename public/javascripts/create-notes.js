e2e365e06dfbc2655e0da49330977e52f7a9f491
John Sextro <john.sextro@gmail.com>
2014-12-01 18:10:16 -0600

    Refactored out hard coded segments and replaced them with the actual segments.

--- public/javascripts/create-notes.js
+++ public/javascripts/create-notes.js
@@ -1,15 +1,15 @@
+var segments = [];
+
 function addSegment(segment, notes) {
   $('#segment-list').append('<div class="panel panel-default"><h4 class="panel-title panel-heading">' 
   	+ segment + '<span class="col-md-offset-10"><span class="glyphicon glyphicon-remove" aria-hidden="true"</span>' 
   	+ '<span class="glyphicon glyphicon-arrow-up" aria-hidden="true"</span>' 
   	+ '<span class="glyphicon glyphicon-arrow-down" aria-hidden="true"</span></span></h4><label>' 
   	+ notes + '</label></div>');
+  segments.push({name: segment, notes: notes, position: segments.length + 1});
 }
 
 function saveNotes() {
-  var segments = [{
-    name: 'Tweet shoutouts', notes: 'First tweet', position: 1},
-    {name: 'Discussion', notes: 'Topic', position: 2}];
   var showData = {showNumber: $('#showNumber').val(), podcast: 1, notes: "Default Notes", showTitle: $('#episodeTitle').val(), segments: segments};
   $.ajax({type: "POST",
     contentType: "application/json; charset=utf-8",
