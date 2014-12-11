f77bf2141744d14b546e4f437773f0fecf803f9d
John Sextro <John.Sextro@gmail.com>
2014-12-11 15:07:34 -0600

    removing unused code

--- public/javascripts/run-show.js
+++ public/javascripts/run-show.js
@@ -89,37 +89,9 @@ function createHandlersForSocketMessages() {
     createEventButton(buttonText);
   });
 }
-
-function signinCallback(authResult) {
-  if (authResult['status']['signed_in']) {
-    // Update the app to reflect a signed in user
-    // Hide the sign-in button now that the user is authorized, for example:
-    document.getElementById('signinButton').setAttribute('style', 'display: none');
-    gapi.client.load('plus','v1', function(){
-     var request = gapi.client.plus.people.get({
-       'userId': 'me'
-     });
-     request.execute(function(resp) {
-       $('#displayName').text(resp.displayName);
-     });
-    });
-  } else {
-    // Update the app to reflect a signed out user
-    // Possible error values:
-    //   "user_signed_out" - User is signed-out
-    //   "access_denied" - User denied access to your app
-    //   "immediate_failed" - Could not automatically log in the user
-    console.log('Sign-in state: ' + authResult['error']);
-  }
-
-}
  
 $(function() {
   createHandlersForSocketMessages();
 
   wireLinksToActions();
-
-  var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-  po.src = 'https://apis.google.com/js/client:plusone.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
 });
